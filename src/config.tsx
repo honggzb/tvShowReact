715ceb14d4905d2c878b6c0c7a8794e61968dd42
Joanna Qian <honggzb@users.noreply.github.com>
2023-12-26 22:32:22 -0500

    Update config.tsx

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,5 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=e6c2185068b28afb290a07c5b20facfd";
+const API_KEY_PARAM = "?api_key=xxx";
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
 const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300";
 
